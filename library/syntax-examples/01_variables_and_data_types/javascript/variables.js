3147b74c33d5fc29e2b6370339e96582bd9ecb8a
Apple <apple@Apples-MacBook-Pro.local>
2023-12-16 18:27:52 +0700

    U1

---
+++ library/syntax-examples/01_variables_and_data_types/javascript/variables.js
@@ -0,0 +1,11 @@
+// Declare variables using var, let, const
+var variableVar = 10;
+let variableLet = "Hello, World!";
+const variableConst = true;
+
+// Display variables
+console.log(variableVar, variableLet, variableConst);
+
+// Reassign a variable
+variableVar = 20;
+console.log(variableVar);
